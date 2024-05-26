c41ec9539e3e2e92f0ef712344619a8cc75c811c
Yi Wu <wymario@163.com>
2024-05-26 19:19:38 +0100

    staged attractiveness computation codes

--- compute.py
+++ compute.py
@@ -1,164 +1,175 @@
-# Computing codes for Odakyu project in terms of POI categorisation and attraction index calculation
+# Codes for Odakyu project in terms of POI utilisation and attraction index calculation
 #
-# Created by Yi on 23 May 2024.
+# Created by Yi on 25 May 2024.
 #
-
-import re
-import openai
-import pandas as pd
-
+import matplotlib.pyplot as plt
 from tqdm import tqdm
 from pathlib import Path
-from collections import Counter
-from ast import literal_eval
 
-from utils import get_timestamp, create_engine, openaix
+import pandas as pd
+import numpy as np
+import statsmodels.api as sm
+
+from scipy.stats import entropy
+from utils import get_timestamp, create_engine
 
 
-def auto_poi_categorise(client: openai.Client,
-                        items: list,
-                        categories: dict = None,
-                        save: Path = Path('poi'),
-                        **kwargs) -> pd.DataFrame:
+def get_stats(d: pd.DataFrame):
+    """Get stats about count/percent in each categories"""
+    c1 = d[['secondary', 'cid']].groupby('secondary').count().reset_index()
+    c2 = d[['primary', 'cid']].groupby('primary').count().reset_index()
+
+    c = d[['primary', 'secondary']].drop_duplicates().merge(c1, on='secondary', how='left')
+    c = c.merge(c2, on='primary', how='left')
+    c = c.sort_values(by=['primary', 'secondary'], ascending=True)
+    c.columns = ['primary', 'secondary', 'secondary#', 'primary#']
+    c['secondary%'] = c['secondary#'] / c['secondary#'].sum()
+    c['primary%'] = c['primary#'] / c['secondary#'].sum()
+    return c
+
+
+def get_poi_by_station(d: pd.DataFrame, dmap: pd.DataFrame, station_id: str, radius: float = 2):
     """
-    Automatically implement POI categorisation through a GPT model
+    Return a subset dataframe with selected POIs
 
-    :param client: an openai Client object
-    :param items: a list of unstructured categories
-    :param categories: the categories for automatic summary, default None, which will enforce GPT to run itself
-    :param save: path for saving the file, default `poi/`
-    :param kwargs: params for GPT, incl. prompt, max_tokens, model, etc.
-    :return: a list of structured categories
+    :param d: the POI dataframe
+    :param dmap: distance map for all POIs and stations
+    :param station_id: a string-like station id, like TO01
+    :param radius: a float radius that limits POIs selected
+    :return:
     """
-    model = 'gpt-4o'
-    results = []
-
-    for i, ent in enumerate(tqdm(items, desc='GPT Translating')):
-        # create the prompt
-        if categories is None:
-            prompt = """Please classify the following map entities into a list of categories and return in the following JSON format: 
-                     {{"primary": "category", "secondary": ["entities"]}}, and make sure every entity should be well classified by its semantic name.
-                     Here is the list of map entities {} separated by ",".""".format(ent)
-        else:
-            prompt = f"""Please learn the semantic mappings in the provided JSON data and the categories: {json.dumps(categories, ensure_ascii=False)}.
-                Classify the provided word into a specific Category as well as a Subcategory. If the word doesn't fit any category, label it as 'Other'. 
-                Return the result in the following JSON format, excluding empty categories: 
-                {{ "Category": {{"Subcategory": ["word"]}} }}. Make sure to exclude any categories or subcategories that are empty.
-                Here is the word: "{ent}" and here are the categories: """
-
-        messages = [{"role": "system",
-                     "content": "You are professional in semantically annotating map entities and point of interests"},
-                    {"role": "user", "content": prompt}]
-        try:
-            feed = openaix(client, text='', messages=messages, model=model)
-            res = [literal_eval(it) for it in re.findall(r'{[\w\W]+}', feed)]
-            results += res
-        except Exception as e:
-            print(f'Error {e} at position: {i}')
-            break
-
-    out = []
-    for i, r in enumerate(results):
-        if 'Category' in r.keys():
-            r = r['Category']
-
-        p = list(r.keys())[0]
-        if isinstance(r[p], list):
-            s = p
-            t = r[p][0]
-        else:
-            s = list(r[p].keys())[0]
-            t = r[p][s][0]
-
-        out += [[p, s, t]]
-
-    out = pd.DataFrame(out, columns=['primary', 'secondary', 'tertiary'])
-    out = out.drop_duplicates()
-    out.to_excel(save / f'auto-poi-categorised-{get_timestamp()}.xlsx', index=False)
-    return out
+    # use station id to extract distance table from database
+    assert station_id in dmap.columns, f'Invalid {station_id} not in dmap!'
+    r = dmap.loc[dmap[station_id] <= radius, ['cid', station_id]]
+    r.columns = ['cid', 'distance']
+    o = d[d['cid'].isin(r['cid'].astype(str).tolist())]
+    o = o.merge(r, on='cid')
+    return o
 
 
-def get_category_list(file: Path, mode: str = 'empty'):
-    """Return a dict of categories incl. primary and secondary categories"""
-    poi = pd.read_excel(file)
-    poi = poi[['primary', 'secondary', 'tertiary']].drop_duplicates()
-    poi = poi[~poi['secondary'].isna()]
+def add_factors(d: pd.DataFrame,
+                dmap: pd.DataFrame,
+                purpose: pd.DataFrame,
+                station: pd.DataFrame,
+                save: Path,
+                radius: float = 2) -> pd.DataFrame:
+    """Add on factors such as accessibility, reviews (place_topics) and rating.
+    The data structure should be a bilateral i->j form and k-th , e.g.
+    stop_i, category_j, X_i, X_j, d_ij, ...
 
-    cate = {}
-    top = poi.sort_values(['primary'])
-    for _, row in top.iterrows():
-        row = row.to_dict()
-        p, s, t = row['primary'], row['secondary'], row['tertiary']
-        if p in cate:  # primary tier
-            if s in cate[p]:
-                if mode == 'empty':
-                    continue
-                elif mode == 'all':
-                    cate[p][s] += [row['tertiary']]
-                else:  # mode='single'
-                    continue
-            else:
-                if mode == 'empty':
-                    cate[p] = {s: []}
-                else:  # mode='single'
-                    cate[p][s] = [row['tertiary']]
-        else:
-            cate[p] = {s: []}
-            if mode == 'empty':
+    :param d: the odakyu dataframe
+    :param dmap: distance map for all POIs and stations
+    :param purpose: the purpose relationship table between population groups and POIs
+    :param station: the station info dataframe
+    :param save: path for saving the dataframe
+    :param radius: a float radius that limits POIs selected
+    """
+    rows = []
+    d['cid'] = d['cid'].astype(str)
+    stops = station['id'].tolist()
+    # for each station, we get poi and then extract variables
+    for stop in tqdm(stops, desc='Building'):
+        p = get_poi_by_station(d, dmap, stop)
+        # for a specific category j
+        for c, g in p.groupby('secondary'):
+            # get Y=reviews (i->j, reviews, inflows)
+            y = g['rating_votes_count'].dropna().mean()
+            if y == np.nan:  # only have unrated POIs
                 continue
-            elif mode == 'all':
-                cate[p][s] += [row['tertiary']]
-            else:  # mode='single'
-                cate[p][s] = [row['tertiary']]
 
-    return cate
+            # NB. alternatively, use high-rating users
+            # g['rater'] = g['rating_distribution'].dropna().apply(lambda x: sum(list(eval(x).values())[:-2]))
+            # y = g['rater'].sum()
+            # get distances
+            di = g['distance'].mean()
+            # get X_i using number of POIs around the station or total passengers
+            xi = len(g)
+            # get ∑X_j*n_jk, k should be extracted from purpose
+            # NB. regardless of the n_jk, will obtain a gross-level indicator
+            # by extending the columns for 6 groups to xj1 ... xj6, the model can be mapped to different groups
+            xj = g['rating_value'].dropna().mean()
+            # get alpha_ij
+            a = entropy(g['rating_value'].dropna().values)
+            if a == 0:
+                continue
+
+            rows += [[stop, c, y, di, a, xi, xj]]
+
+    # create the dataset
+    dat = pd.DataFrame(rows, columns=['station', 'category', 'y', 'distance', 'alpha', 'xi', 'xj'])
+    # convert variables
+    dat['y'] = np.log(dat['y'])
+    dat['distance'] = np.log(dat['distance'])
+    dat['alpha'] = np.log(dat['alpha'])
+    dat['xi'] = np.log(dat['xi'])
+    dat['xj'] = np.log(dat['xj'])
+    # run baseline model for estimating the overall attractiveness
+    dat = dat.dropna(how='any')
+    model = sm.OLS(dat['y'], dat[['distance', 'alpha', 'xi', 'xj']])
+    results = model.fit(cov_type='HC1')
+    print(results.summary())
+
+    # run subsamples for station-specific attractiveness
+    xjs = []
+    for stop, g in tqdm(dat.groupby('station'), desc='Subsample fitting'):
+        m = sm.OLS(g['y'], g[['distance', 'alpha', 'xi', 'xj']])
+        fitted = m.fit(cov_type='HC1')
+        xjs += [[stop, fitted.params.loc['xj']]]
+
+    att = pd.DataFrame(xjs, columns=['id', 'att'])
+    att = att.merge(station[['id', 'name']], on='id', how='left')
+    att = att.sort_values('att', ascending=True)
+
+    fig = plt.figure(figsize=(8, 10))
+    plt.plot(att.att.values, range(len(att)))
+    plt.yticks(range(len(att)), att.name.tolist(), rotation=0)
+    plt.tight_layout()
+    plt.margins(0.01)
+    fig.savefig('result/attraction.png', format='png', dpi=100)
+    plt.show()
+    return dat
+
+
+def gravity_model(d: pd.DataFrame):
+    """
+    Modified / advanced model where a few changes could be made,
+    - distance could be adjusted by price_level
+    - opening time / popular times
+    - accessibility (wheels)
+
+    :param d:
+    :return:
+    """
+
+    return
 
 
 if __name__ == '__main__':
-    import json
+    # global conf
+    save = Path('result')
 
-    # general config
-    save = Path('poi')
+    # NB. might use the additional data for calculation of attraction
+    oda_file = Path('poi') / 'categories-odakyu-poi-2k-2024-05-25 10-43-38.csv'
+    oda = pd.read_csv(oda_file)
 
-    # load poi data
+    # get statistics
+    stat = get_stats(oda)
+    stat.to_excel(save / f'stats-categories-{get_timestamp()}.xlsx', index=False)
+
+    # how to utilise the reviews and rating info
+    drop_keys = ['feature_id', 'address_info_borough', 'address_info_address',
+                 'address_info_zip', 'address_info_country_code', 'place_id', 'phone',
+                 'domain', 'main_image', 'snippet', 'is_claimed', 'additional_categories',
+                 'time_update', 'check_url']
+    oda = oda.drop(columns=drop_keys)
+    head = oda.head(100)
+
+    # processing attribute for accessibility + reservation/book/menu as convenience using
+    # `local_business_links` column + `url` + `contacts`
     sqlite = 'sqlite:///data/tokyo-poi.db'
     engine = create_engine(sqlite)
-    # retrieve POI table and deduplicate and the correct number of POIs is about 193,728!
-    oda = pd.read_sql_table('odakyu', engine)
-    oda = oda.drop(columns='timestamp')  # entirely empty
-    oda = oda[~oda[['title', 'description']].duplicated()]
-    # retrieve unique POI categories
-    cate = oda.loc[~oda['category'].isna(), 'category'].values.tolist()
-    uni_cate = Counter(cate)  # a key-value formed counted return
-    uni_cate = pd.DataFrame(uni_cate, index=[0]).T.reset_index()
-    uni_cate.columns = ['category', 'count']
-    uni_cate.to_excel(f'poi/poi-category-count-{get_timestamp()}.xlsx', index=False)
-
-    # load ai config
-    conf = json.loads(Path('conf.json').read_text(encoding='utf8'))
-    openai_api_key = conf['openai-api-key']
-    client = openai.Client(api_key=openai_api_key)
-
-    opt_poi_file = Path('poi') / 'optimal-poi-categorised.xlsx'
-    poi = pd.read_excel(opt_poi_file)
-    ratio = poi.loc[poi['count'] < 10, 'count'].sum() / poi['count'].sum()
-    print(f'Use <10 as threshold, {round(ratio, 3)*100}% of entries have been deleted')
-    poi = poi[poi['count'] >= 10]  # remove those with less than 10 POIs
-
-    mask = poi[['primary', 'secondary']].duplicated()
-    top = poi.loc[~mask, ['primary', 'secondary']]
-    top.sort_values('primary').to_excel(Path('poi') / 'category-list.xlsx', index=False)
-
-    # test auto-labelling with GPT
-    categories = get_category_list(opt_poi_file, mode='empty')
-    ents = poi.tertiary.tolist()
-    results = auto_poi_categorise(client, ents[:1], categories)
-
-    # use oda to find all annotated POIs
-    poi.columns = ['category', 'primary', 'secondary', 'count']
-    print(f'Unique three-tiered POIs categories have: {len(poi)} entries!')
-    selected = oda[oda['category'].isin(poi['category'].tolist())]
-    selected = selected.merge(poi[['category', 'primary', 'secondary']], on='category', how='left')
-    selected.to_csv(save / f'categories-odakyu-poi-2k-{get_timestamp()}.csv', index=False)
-    categories = get_category_list(opt_poi_file, mode='all')
-    (save / f'categories-all-{get_timestamp()}.json').write_text(json.dumps(categories))
+    dmap = pd.read_sql_table('odakyu2k', engine)  # you DO NOT have to use odakyu1k because it's a subset of odakyu2k
+    # get stations
+    station = pd.read_excel('result/odakyu-stops-final.xlsx')
+    stops = station['id'].tolist()
+    poi = get_poi_by_station(oda, dmap, stops[0])
---
+++ poi/poi.py
@@ -0,0 +1,164 @@
+# Codes for Odakyu project in terms of POI categorisation
+#
+# Created by Yi on 23 May 2024.
+#
+
+import re
+import openai
+import pandas as pd
+
+from tqdm import tqdm
+from pathlib import Path
+from collections import Counter
+from ast import literal_eval
+
+from utils import get_timestamp, create_engine, openaix
+
+
+def auto_poi_categorise(client: openai.Client,
+                        items: list,
+                        categories: dict = None,
+                        save: Path = Path(''),
+                        **kwargs) -> pd.DataFrame:
+    """
+    Automatically implement POI categorisation through a GPT model
+
+    :param client: an openai Client object
+    :param items: a list of unstructured categories
+    :param categories: the categories for automatic summary, default None, which will enforce GPT to run itself
+    :param save: path for saving the file, default `poi/`
+    :param kwargs: params for GPT, incl. prompt, max_tokens, model, etc.
+    :return: a list of structured categories
+    """
+    model = 'gpt-4o'
+    results = []
+
+    for i, ent in enumerate(tqdm(items, desc='GPT Translating')):
+        # create the prompt
+        if categories is None:
+            prompt = """Please classify the following map entities into a list of categories and return in the following JSON format: 
+                     {{"primary": "category", "secondary": ["entities"]}}, and make sure every entity should be well classified by its semantic name.
+                     Here is the list of map entities {} separated by ",".""".format(ent)
+        else:
+            prompt = f"""Please learn the semantic mappings in the provided JSON data and the categories: {json.dumps(categories, ensure_ascii=False)}.
+                Classify the provided word into a specific Category as well as a Subcategory. If the word doesn't fit any category, label it as 'Other'. 
+                Return the result in the following JSON format, excluding empty categories: 
+                {{ "Category": {{"Subcategory": ["word"]}} }}. Make sure to exclude any categories or subcategories that are empty.
+                Here is the word: "{ent}" and here are the categories: """
+
+        messages = [{"role": "system",
+                     "content": "You are professional in semantically annotating map entities and point of interests"},
+                    {"role": "user", "content": prompt}]
+        try:
+            feed = openaix(client, text='', messages=messages, model=model)
+            res = [literal_eval(it) for it in re.findall(r'{[\w\W]+}', feed)]
+            results += res
+        except Exception as e:
+            print(f'Error {e} at position: {i}')
+            break
+
+    out = []
+    for i, r in enumerate(results):
+        if 'Category' in r.keys():
+            r = r['Category']
+
+        p = list(r.keys())[0]
+        if isinstance(r[p], list):
+            s = p
+            t = r[p][0]
+        else:
+            s = list(r[p].keys())[0]
+            t = r[p][s][0]
+
+        out += [[p, s, t]]
+
+    out = pd.DataFrame(out, columns=['primary', 'secondary', 'tertiary'])
+    out = out.drop_duplicates()
+    out.to_excel(save / f'auto-poi-categorised-{get_timestamp()}.xlsx', index=False)
+    return out
+
+
+def get_category_list(file: Path, mode: str = 'empty'):
+    """Return a dict of categories incl. primary and secondary categories"""
+    poi = pd.read_excel(file)
+    poi = poi[['primary', 'secondary', 'tertiary']].drop_duplicates()
+    poi = poi[~poi['secondary'].isna()]
+
+    cate = {}
+    top = poi.sort_values(['primary'])
+    for _, row in top.iterrows():
+        row = row.to_dict()
+        p, s, t = row['primary'], row['secondary'], row['tertiary']
+        if p in cate:  # primary tier
+            if s in cate[p]:
+                if mode == 'empty':
+                    continue
+                elif mode == 'all':
+                    cate[p][s] += [row['tertiary']]
+                else:  # mode='single'
+                    continue
+            else:
+                if mode == 'empty':
+                    cate[p] = {s: []}
+                else:  # mode='single'
+                    cate[p][s] = [row['tertiary']]
+        else:
+            cate[p] = {s: []}
+            if mode == 'empty':
+                continue
+            elif mode == 'all':
+                cate[p][s] += [row['tertiary']]
+            else:  # mode='single'
+                cate[p][s] = [row['tertiary']]
+
+    return cate
+
+
+if __name__ == '__main__':
+    import json
+
+    # general config
+    save = Path('')
+
+    # load poi data
+    sqlite = 'sqlite:///data/tokyo-poi.db'
+    engine = create_engine(sqlite)
+    # retrieve POI table and deduplicate and the correct number of POIs is about 193,728!
+    oda = pd.read_sql_table('odakyu', engine)
+    oda = oda.drop(columns='timestamp')  # entirely empty
+    oda = oda[~oda[['title', 'description']].duplicated()]
+    # retrieve unique POI categories
+    cate = oda.loc[~oda['category'].isna(), 'category'].values.tolist()
+    uni_cate = Counter(cate)  # a key-value formed counted return
+    uni_cate = pd.DataFrame(uni_cate, index=[0]).T.reset_index()
+    uni_cate.columns = ['category', 'count']
+    uni_cate.to_excel(save / f'poi-category-count-{get_timestamp()}.xlsx', index=False)
+
+    # load ai config
+    conf = json.loads(Path('conf.json').read_text(encoding='utf8'))
+    openai_api_key = conf['openai-api-key']
+    client = openai.Client(api_key=openai_api_key)
+
+    opt_poi_file = save / 'optimal-poi-categorised.xlsx'
+    poi = pd.read_excel(opt_poi_file)
+    ratio = poi.loc[poi['count'] < 10, 'count'].sum() / poi['count'].sum()
+    print(f'Use <10 as threshold, {round(ratio, 3) * 100}% of entries have been deleted')
+    poi = poi[poi['count'] >= 10]  # remove those with less than 10 POIs
+
+    mask = poi[['primary', 'secondary']].duplicated()
+    top = poi.loc[~mask, ['primary', 'secondary']]
+    top.sort_values('primary').to_excel(Path('poi') / 'category-list.xlsx', index=False)
+
+    # test auto-labelling with GPT
+    categories = get_category_list(opt_poi_file, mode='empty')
+    ents = poi.tertiary.tolist()
+    results = auto_poi_categorise(client, ents[:1], categories)
+
+    # use oda to find all annotated POIs
+    poi.columns = ['category', 'primary', 'secondary', 'count']
+    print(f'Unique three-tiered POIs categories have: {len(poi)} entries!')
+    selected = oda[oda['category'].isin(poi['category'].tolist())]
+    selected = selected.merge(poi[['category', 'primary', 'secondary']], on='category', how='left')
+    selected.to_csv(save / f'categories-odakyu-poi-2k-{get_timestamp()}.csv', index=False)
+    categories = get_category_list(opt_poi_file, mode='all')
+    (save / f'categories-all-{get_timestamp()}.json').write_text(json.dumps(categories))
